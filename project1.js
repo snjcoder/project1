bf1f11a88c63ac7494d05e65c16bb07c460fcf2e
Neetha <neethacohort@gmail.com>
2022-03-11 14:39:13 -0700

    added app

--- project1.js
+++ project1.js
@@ -1,3 +1,6 @@
+const readlinesync = require("readline-sync")
+let gameName = readlinesync.question("Please enter game? ");
+console.log("Hangman",gameName);
 
 let randomWord = ['otter', 'hippo', 'whale', 'dolphin', 'shark', 'polar bear', 'platypus', 'walrus', 'killer whale', 'seal', 'sea mink'];
 
--- server.js
+++ server.js
@@ -1,3 +1,19 @@
-const readlinesync = require("readline-sync")
-let gameName = readlinesync.question("Please enter game? ");
-console.log("Hangman",gameName);
+const express = require("express");
+const app = express();
+const fs=require('fs');
+const PORT = 7000;
+
+app.listen(PORT, function() { 
+     console.log(`Listening on port ${PORT}`);
+   
+});
+
+app.get("/startGame", (req,res) =>{ 
+    
+    res.send({"Hangman":"Game"})
+});
+app.get("/startGame", (req,res) =>{ 
+    
+    res.send({"Hangman":"Game"})
+});
+
